99be40029495bc7bc554a185893a4f8173058c92
Gerssonn <gersonhernan00@hotmail.com>
2023-09-16 22:40:19 -0300

    jeje

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/HZzPIkLZQM8"
+            src="https://youtu.be/ZLaRNBYgRjs"
             title="YouTube MUVEC"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
